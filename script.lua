9b60fdd255396c4c3559d5417cac7a6080b815fe
Vinciith2 <77860480+Vinciith2@users.noreply.github.com>
2022-01-12 11:14:04 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,14 +1,98 @@
+--[[
+--basic script
+local lib = library:CreateMain()
 
-local URL = "https://solarishub.dev/games"
+lib:CreateButton(
+	"Best ui lib",
+	function()
+		print("button clicked")
+	end
+)
 
-local games = {
-	[292439477] = "Phantom Forces",
-}
+]]
 
-for i,v in next, games do
-	games[i] = table.concat(v:split(' '), '_')
+local Library = {}
+
+function Library:CreateMain()
+	local ScreenGui = Instance.new("ScreenGui")
+	local Frame = Instance.new("ScrollingFrame")
+	local UIListLayout = Instance.new("UIListLayout")
+
+	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+	Frame.Parent = ScreenGui
+	Frame.BackgroundColor3 = Color3.fromRGB(63, 63, 63)
+	Frame.BorderSizePixel = 0
+	Frame.Position = UDim2.new(0.351063818, 0, 0.312734097, 0)
+	Frame.Size = UDim2.new(0, 324, 0, 187)
+
+	UIListLayout.Parent = Frame
+	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
+	local GamerLibrary = {}
+
+	function GamerLibrary:NewButton(Text, Func)
+		local TextButton = Instance.new("TextButton")
+
+		TextButton.Parent = Frame
+		TextButton.BackgroundColor3 = Color3.fromRGB(25,25,25)
+		TextButton.BackgroundTransparency = .8
+		TextButton.Size = UDim2.new(1,0, 0, 50)
+		TextButton.Font = Enum.Font.ArialBold
+		TextButton.Text = Text
+		TextButton.TextColor3 = Color3.fromRGB(255,255,255)
+		TextButton.TextScaled = true
+		Frame.CanvasSize = UDim2.new(0,0,Frame.CanvasSize.Y.Scale + .8,0)
+
+		TextButton.MouseButton1Click:Connect(function()
+			spawn(function() Func() end)
+		end)
+	end
+	
+	function GamerLibrary:NewLabel(Text)
+		local TextButton = Instance.new("TextLabel")
+		local UIStroke = Instance.new("UIStroke")
+
+		TextButton.Parent = Frame
+		TextButton.BackgroundColor3 = Color3.fromRGB(25,25,25)
+		TextButton.BackgroundTransparency = .8
+		TextButton.Size = UDim2.new(1,0, 0, 50)
+		TextButton.Font = Enum.Font.ArialBold
+		TextButton.Text = Text
+		TextButton.TextColor3 = Color3.fromRGB(255,255,255)
+		TextButton.TextScaled = true
+		Frame.CanvasSize = UDim2.new(0,0,Frame.CanvasSize.Y.Scale + .8,0)
+		
+		UIStroke.Parent = TextButton
+		UIStroke.Color = Color3.new(1,1,1)
+		UIStroke.Thickness = 3
+	end
+	
+	function GamerLibrary:NewToggle(Text, Value, Func)
+		local TextButton = Instance.new("TextButton")
+
+		TextButton.Parent = Frame
+		TextButton.BackgroundColor3 = Color3.fromRGB(25,25,25)
+		TextButton.BackgroundTransparency = .8
+		TextButton.Size = UDim2.new(1,0, 0, 50)
+		TextButton.Font = Enum.Font.ArialBold
+		TextButton.Text = Text
+		TextButton.TextColor3 = Color3.fromRGB(255, 0, 0)
+		TextButton.TextScaled = true
+		Frame.CanvasSize = UDim2.new(0,0,Frame.CanvasSize.Y.Scale + .8,0)
+
+		TextButton.MouseButton1Click:Connect(function()
+			Value = Func(not Value)
+			if Value then
+				TextButton.TextColor3 = Color3.fromRGB(0, 255, 0)
+			else
+				TextButton.TextColor3 = Color3.fromRGB(255, 0, 0)
+			end
+		end)
+	end
+
+	return GamerLibrary
 end
 
-local name = games[game.PlaceId] or games[game.GameId]
-
-return loadstring(game:HttpGet(URL .. "/" .. (name or "Universal") .. ".lua", true))()
+return Library;
